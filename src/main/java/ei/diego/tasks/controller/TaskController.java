218bf3caf261f8a3b542d0f3fcca6e8ced77ba3c
Diego R. Barbosa <diegokod@gmail.com>
2024-04-06 17:23:31 -0300

    Feat: Creating DTOs and methods to convert DTO to model with Builder Pattern

--- src/main/java/ei/diego/tasks/controller/TaskController.java
+++ src/main/java/ei/diego/tasks/controller/TaskController.java
@@ -1,5 +1,7 @@
 package ei.diego.tasks.controller;
 
+import ei.diego.tasks.controller.converter.TaskDTOConverter;
+import ei.diego.tasks.controller.dto.TaskDTO;
 import ei.diego.tasks.model.Task;
 import ei.diego.tasks.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,19 +16,23 @@ public class TaskController {
 
     private final TaskService service;
 
+    private final TaskDTOConverter converter;
 
-    public TaskController(TaskService service) {
+    public TaskController(TaskService service, TaskDTOConverter converter) {
         this.service = service;
+        this.converter = converter;
     }
 
     @GetMapping
-    public Mono<List<Task>> getTasks() {
-        return service.list();
+    public Mono<List<TaskDTO>> getTasks() {
+        return service.list()
+                .map(converter::convertList);
     }
 
     @PostMapping
-    public Mono<Task> createTask(@RequestBody Task task){
-        return service.insert(task);
+    public Mono<TaskDTO> createTask(@RequestBody Task task){
+        return service.insert(task)
+                .map(converter::convert);
     }
 
 }
